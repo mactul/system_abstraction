d76f9135c57e214b9b976d23c847403168f42f60
mactul <mactulgames@gmail.com>
2024-01-19 14:30:32 +0100

    avoid segfault on windows destruction

--- SA/graphics/x11/draw.c
+++ SA/graphics/x11/draw.c
@@ -6,6 +6,10 @@
 
 SA_bool SA_graphics_vram_draw_pixel(SA_GraphicsWindow* window, uint32_t x, uint32_t y, uint32_t color)
 {
+    if(window->is_killed)
+    {
+        return SA_FALSE;
+    }
     pthread_mutex_lock(&(window->mutex));
     XSetForeground(window->display, window->gc, color);
     XDrawPoint(window->display, window->vram, window->gc, x, y);
@@ -26,6 +30,10 @@ SA_bool SA_graphics_vram_draw_vertical_line(SA_GraphicsWindow* window, uint32_t 
 
 SA_bool SA_graphics_vram_draw_line(SA_GraphicsWindow* window, int32_t x1, int32_t y1, int32_t x2, int32_t y2, uint32_t color, uint16_t thickness)
 {
+    if(window->is_killed)
+    {
+        return SA_FALSE;
+    }
     pthread_mutex_lock(&(window->mutex));
     XSetForeground(window->display, window->gc, color);
     XSetLineAttributes(window->display, window->gc, thickness, LineSolid, CapNotLast, JoinMiter);
@@ -37,6 +45,10 @@ SA_bool SA_graphics_vram_draw_line(SA_GraphicsWindow* window, int32_t x1, int32_
 
 SA_bool SA_graphics_vram_draw_rectangle(SA_GraphicsWindow* window, uint32_t x, uint32_t y, uint32_t width, uint32_t height, uint32_t color)
 {
+    if(window->is_killed)
+    {
+        return SA_FALSE;
+    }
     pthread_mutex_lock(&(window->mutex));
     XSetForeground(window->display, window->gc, color);
     XFillRectangle(window->display, window->vram, window->gc, x, y, width, height);
@@ -47,6 +59,10 @@ SA_bool SA_graphics_vram_draw_rectangle(SA_GraphicsWindow* window, uint32_t x, u
 
 SA_bool SA_graphics_vram_draw_text(SA_GraphicsWindow* window, uint32_t x, uint32_t y, const char* str, uint32_t color)
 {
+    if(window->is_killed)
+    {
+        return SA_FALSE;
+    }
     pthread_mutex_lock(&(window->mutex));
     XSetForeground(window->display, window->gc, color);
     XDrawString(window->display, window->vram, window->gc, x, y, str, SA_strlen(str));
@@ -57,6 +73,10 @@ SA_bool SA_graphics_vram_draw_text(SA_GraphicsWindow* window, uint32_t x, uint32
 
 void SA_graphics_vram_flush(SA_GraphicsWindow* window)
 {
+    if(window->is_killed)
+    {
+        return;
+    }
     pthread_mutex_lock(&(window->mutex));
     XCopyArea(window->display, window->vram, window->renderer, window->gc, 0, 0, window->width, window->height, 0, 0);
     XCopyArea(window->display, window->vram, window->window, window->gc, 0, 0, window->width, window->height, 0, 0);
