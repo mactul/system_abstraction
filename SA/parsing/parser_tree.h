842f460127926d694e2a9af8c15a2cdaf7785aca
mactul <mactulgames@gmail.com>
2024-01-26 14:19:32 +0100

    improve documentation for parsing

--- SA/parsing/parser_tree.h
+++ SA/parsing/parser_tree.h
@@ -8,14 +8,90 @@
     extern "C"{
     #endif
 
+    /**
+     * @brief Create a new ParserTree
+     * 
+     * @return - When it succededs, it returns a pointer to a ParserTree handler.
+     * @return - When it fails, it returns NULL and `SA_print_last_error` can tell what happened
+     */
     SA_ParserTree* SA_ptree_init();
+
+
+    /**
+     * @brief Allocate the space for the partial_key and concatenate it to the older one.  
+     * @brief It can be called multiple time if the key you want to store is separated in multiples chunks.  
+     * @brief However, because of the realloc, it's quicker if you work with large chunks.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     * @param partial_key a string that is the part of the key you are actually parsing
+     * @param partial_key_len the strlen of the key (can be used to limit the size of the buffer)
+     * @return - When it succeded, it returns SA_TRUE.
+     * @return - When it fails, it's a memory error, the operation is aborted and the previously allocated key is freed.
+     */
     SA_bool SA_ptree_update_key(SA_ParserTree* tree, const char* partial_key, int partial_key_len);
+
+
+    /**
+     * @brief Allocate the space for the partial_value and concatenate it to the older one.  
+     * @brief It can be called multiple time if the value you want to store is separated in multiples chunks.  
+     * @brief However, because of the realloc, it's quicker if you work with large chunks.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     * @param partial_value a string that is the part of the value you are actually parsing
+     * @param partial_value_len the strlen of the value (can be used to limit the size of the buffer)
+     * @return - When it succeded, it returns SA_TRUE.
+     * @return - When it fails, it's a memory error, the operation is aborted and the previously allocated value is freed.
+     */
     SA_bool SA_ptree_update_value(SA_ParserTree* tree, const char* partial_value, int partial_value_len);
+
+
+    /**
+     * @brief Once you have added your key and your value,  
+     * @brief you have to call SA_ptree_push to add this couple to the final tree and start saving something else.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     * @param data_modifications a pointer to a function which can operate on the key and the value. For example, you can decode a base64 or an urlencoded string at this moment.
+     * @return SA_TRUE when it succeed, otherwise, SA_FALSE.
+     */
     SA_bool SA_ptree_push(SA_ParserTree* tree, void (*data_modifications)(char*));
+
+
+    /**
+     * @brief Once the parsing is done, use this to get the value associated with KEY (KEY is case unsensitive).  
+     * @brief If you have to modify this value, copy it before.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     * @param key the key corresponding to the value registered
+     * @return - when it succededs, it returns the value registered
+     * @return - when it fails, it returns NULL
+     */
     const char* SA_FUNCTION_PURE SA_ptree_get_value(SA_ParserTree* tree, const char* key);
+
+
+    /**
+     * @brief Free the tree and set the tree handler to NULL.
+     * 
+     * @param tree the address of the tree handler.
+     */
     void SA_ptree_free(SA_ParserTree** tree);
+
+
+    /**
+     * @brief Abort the saving of current key/value.  
+     * @brief This doesn't free the tree and you can continue to fill it after that.  
+     * @brief You can use this in case you realize while parsing that the current partial key/values you are saving aren't wanted.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     */
     void SA_ptree_abort(SA_ParserTree* tree);
 
+
+    /**
+     * @brief Display the tree.  
+     * Used for debug purpose.
+     * 
+     * @param tree The handler returned by `SA_ptree_init`
+     */
     void SA_ptree_display(SA_ParserTree* tree);
 
     #ifdef __cplusplus
--- SA/parsing/parsing.h
+++ SA/parsing/parsing.h
@@ -3,37 +3,83 @@
     #include "SA/global/global.h"
     #include "SA/parsing/parser_tree.h"
 
-    #define SA_MAX_CHAR_ON_HOST 253  /* this is exact, don't change */
-    #define SA_MAX_URI_LENGTH  1024 /* this can be changed, it's the maximum length a url can have */
+    #define SA_MAX_CHAR_ON_HOST 253 /* this is exact, don't change */
+    #define SA_MAX_URI_LENGTH 1024  /* this can be changed, it's the maximum length a url can have */
 
-    typedef struct _SA_url_splitted {
+    typedef struct _SA_url_splitted
+    {
         char host[SA_MAX_CHAR_ON_HOST + 1];
         char uri[SA_MAX_URI_LENGTH + 1];
         uint16_t port;
         SA_bool secured;
     } SA_UrlSplitted;
 
-    SA_bool SA_parse_url(const char* url, SA_UrlSplitted* url_splitted);
-
     #ifdef __cplusplus
-    extern "C"{
+    extern "C"
+    {
     #endif
-    
+
+
+    /**
+     * @brief split `url` into host, uri, port and wheter or not it's http or https. Fill the structure `url_splitted` with these infos.
+     *
+     * @param url The url to split
+     * @param url_splitted A pointer to SA_UrlSplitted structure that will be filled with the url components.
+     * @return - When it succededs, it returns SA_TRUE
+     * @return - When it fails, it returns SA_FALSE and `SA_print_last_error()` can tell what happends.
+     */
+    SA_bool SA_parse_url(const char *url, SA_UrlSplitted *url_splitted);
+
+
+    /**
+     * @brief This is used in combinaison of `SA_parser_search_occurence_in_bytes_stream`. Call this to specify that you are searching in new stream.
+     *
+     * @note This function is NOT thread safe.
+     */
     void SA_parser_reset_stream(void);
-    SA_bool SA_parser_search_occurence_in_bytes_stream(char stream_single_byte, const char* occurence);
 
-    SA_ParserTree* SA_parse_urlencoded_form(const char* data);
 
-    void SA_urldecode(char* dest, const char* src);
+    /**
+     * @brief This function is used to search an occurence in a stream, byte per byte.
+     *
+     * @param stream_single_byte The byte you are actually reading.
+     * @param occurence The string you are searching for.
+     * @return - SA_FALSE if you have to continue searching.
+     * @return - SA_TRUE if the string `occurence` was encountered.
+     *
+     * @note This function is NOT thread safe.
+     */
+    SA_bool SA_parser_search_occurence_in_bytes_stream(char stream_single_byte, const char *occurence);
+
+
+    /**
+     * @brief This parse datas encoded like `key1=value1&key2=value2&...`
+     *
+     * @param data the string to parse
+     * @return A ParserTree which can be read by `SA_ptree...` functions.
+     */
+    SA_ParserTree *SA_parse_urlencoded_form(const char *data);
+
+    
+    /**
+     * @brief Decode the urlencoded string in `src` to the buffer `dest`.
+     * 
+     * @param dest the destination string, should be at least the size of SRC (urlencoded string are equals or bigger than decoded ones).
+     * @param src the source (encoded) string
+     */
+    void SA_urldecode(char *dest, const char *src);
 
     #ifdef __cplusplus
     }
     #endif
 
-    /*
-    Decode an urlencoded string inplace (urlencoded string are equals or bigger than decoded ones).
-    */
-    static inline void SA_urldecode_inplace(char* data)
+
+    /**
+     * @brief Decode an urlencoded string inplace (urlencoded string are equals or bigger than decoded ones).
+     * 
+     * @param data the string to decode
+     */
+    static inline void SA_urldecode_inplace(char *data)
     {
         SA_urldecode(data, data);
     }
